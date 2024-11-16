98a2fa2a244cea5f68656987d9ca6863ce723e74
Vahagn <vahagn99ghukasyan@gmail.com>
2024-11-17 01:50:11 +0400

    docker

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -2,8 +2,5 @@
 # Start php-fpm in the background
 php-fpm -D
 
-# Запуск cron в фоновом режиме
-crond -f -L /var/log/cron.log &
-
 # Start supervisord to manage Horizon and other processes
 exec supervisord -c /etc/supervisord.conf
